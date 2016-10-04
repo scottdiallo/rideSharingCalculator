49186604defced22bc3b367b47bc81bb273877a1
Scott Diallo <diallot@me.com>
2016-10-04 08:49:14 -0700

    bug fixed

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('waitstaff', ['ngAnimate', 'ngRoute']);
+var app = angular.module('rideSharingCalculator', ['ngAnimate', 'ngRoute']);
 
 //define all routes below
 app.config(['$routeProvider', function ($routeProvider) {
@@ -20,35 +20,35 @@ app.config(['$routeProvider', function ($routeProvider) {
         });
 }]);
 
-app.service('mealDataService', function () {
-    //empty arrays for meals
-    var meals = [];
+app.service('rideDataService', function () {
+    //empty arrays for rides
+    var rides = [];
 
     //cumulative data object
     var cumulativeData = {
-        mealCount: 0,
+        rideCount: 0,
         tipTotal: 0,
         tipAvg: 0
     };
 
 
     return {
-        addMeal: function (meal) {
-            meals.push(meal);
-            cumulativeData.mealCount++;
-            cumulativeData.tipTotal += meal.tip;
-            cumulativeData.tipAvg = (cumulativeData.tipTotal / cumulativeData.mealCount);
+        addRide: function (ride) {
+            rides.push(ride);
+            cumulativeData.rideCount++;
+            cumulativeData.tipTotal += ride.tip;
+            cumulativeData.tipAvg = (cumulativeData.tipTotal / cumulativeData.rideCount);
         },
-        getMeals: function () {
-            return meals;
+        getRides: function () {
+            return rides;
         },
         getCumulativeData: function () {
             return cumulativeData;
         },
         resetAll: function () {
-            meals.length = 0;
+            rides.length = 0;
             cumulativeData = {
-                mealCount: 0,
+                rideCount: 0,
                 tipTotal: 0,
                 tipAvg: 0
             };
@@ -56,102 +56,102 @@ app.service('mealDataService', function () {
     };
 });
 
-app.controller('detailsController', function ($scope, mealDataService) {
-    $scope.mealCount = 1;
-    //get the current meal count when naviating from other view
-    $scope.getMealCounts = function () {
-        var meals = mealDataService.getMeals();
-        console.log(meals);
-        if (typeof meals != "undefined" && meals !== null && meals.length > 0) {
-            $scope.mealCount = meals.length + 1;
+app.controller('detailsController', function ($scope, rideDataService) {
+    $scope.rideCount = 1;
+    //get the current ride count when naviating from other view
+    $scope.getRideCounts = function () {
+        var rides = rideDataService.getRides();
+        console.log(rides);
+        if (typeof rides != "undefined" && rides !== null && rides.length > 0) {
+            $scope.rideCount = rides.length + 1;
         }
     };
 
-    $scope.getMealCounts();
+    $scope.getRideCounts();
 
-    //Clears form for next meal
+    //Clears form for next ride
     $scope.cancelForm = function () {
         $scope.price = '';
         $scope.tax = '';
         $scope.tip = '';
     };
 
-    //Add the meal when 'Add Meal' Is clicked
+    //Add the ride when 'Add ride' Is clicked
     $scope.addTransaction = function () {
-        $scope.mealCount++;
+        $scope.rideCount++;
         var price = parseFloat($scope.price);
         var tax = parseFloat($scope.tax);
         var tip = parseFloat($scope.tip);
 
-        //collecting data form current meal prior to sending to service array
+        //collecting data form current ride prior to sending to service array
         $scope.currentSubtotal = price + (price * (tax / 100));
 
         //the tip is not including the tax
         $scope.currentTip = price * (tip / 100);
         $scope.currentTotal = $scope.currentSubtotal + $scope.currentTip;
 
-        //sending current meal to array of meals in service
-        var meal = {
+        //sending current ride to array of rides in service
+        var ride = {
             subtotal: $scope.currentSubtotal,
             tip: $scope.currentTip,
             total: $scope.currentTotal
         };
-        console.log(meal);
-        mealDataService.addMeal(meal);
+        console.log(ride);
+        rideDataService.addRide(ride);
 
-        //clearing fields for next meal input
+        //clearing fields for next ride input
         $scope.cancelForm();
     };
 
 });
 
-app.controller('chargesController', function ($scope, mealDataService) {
-    $scope.getMeals = function () {
-        var meals = mealDataService.getMeals();
-        $scope.meals = meals;
-        // console.log($scope.meals);
+app.controller('chargesController', function ($scope, rideDataService) {
+    $scope.getRides = function () {
+        var rides = rideDataService.getRides();
+        $scope.rides = rides;
+        // console.log($scope.rides);
     };
 
-    $scope.getMeals();
+    $scope.getRides();
 
-    $scope.mealCount = $scope.meals.length;
+    $scope.rideCount = $scope.rides.length;
 
     $scope.avoidZero = function () {
-        if ($scope.mealCount === 0) {
-            $scope.mealCount = 1;
+        if ($scope.rideCount === 0) {
+            $scope.rideCount = 1;
         }
     };
 
     $scope.avoidZero();
 
-    //back button functionality on click - navigates all meals added
+    //back button functionality on click - navigates all rides added
     $scope.back = function () {
-        // console.log($scope.mealCount);
-        if ($scope.mealCount > 1) {
-            $scope.mealCount--;
+        // console.log($scope.rideCount);
+        if ($scope.rideCount > 1) {
+            $scope.rideCount--;
         }
     };
 
-    // forward button functionality on click - navigates all meals added
+    // forward button functionality on click - navigates all rides added
     $scope.forward = function () {
-        // console.log($scope.mealCount);
-        if ($scope.mealCount < $scope.meals.length) {
-            $scope.mealCount++;
+        // console.log($scope.rideCount);
+        if ($scope.rideCount < $scope.rides.length) {
+            $scope.rideCount++;
         }
     };
 
 });
 
-app.controller('earningsController', function ($scope, mealDataService) {
-    $scope.tipTotal = mealDataService.getCumulativeData().tipTotal;
-    $scope.mealCount = mealDataService.getCumulativeData().mealCount;
-    $scope.tipAvg = mealDataService.getCumulativeData().tipAvg;
+app.controller('earningsController', function ($scope, rideDataService) {
+    $scope.tipTotal = rideDataService.getCumulativeData().tipTotal;
+    $scope.rideCount = rideDataService.getCumulativeData().rideCount;
+    $scope.tipAvg = rideDataService.getCumulativeData().tipAvg;
 });
 
-app.controller('resetCtrl', function ($scope, mealDataService) {
+app.controller('resetCtrl', function ($scope, rideDataService) {
     //When click reset btn
     $scope.resetAll = function () {
-        mealDataService.resetAll();
+        rideDataService.resetAll();
     };
 });
 
@@ -169,69 +169,3 @@ app.controller('navCtrl', function ($scope, $location) {
         }
     };
 });
-
-
-
-//myApp.controller('waitCtrl', ['$scope', function ($scope) {
-//
-//    $scope.resetDetails = function () {
-//        $scope.mealPrice = '';
-//        $scope.taxRate = '';
-//        $scope.tipPercentage = '';
-//    };
-//
-//    $scope.initCharges = function () {
-//        $scope.subtotal = 0;
-//        $scope.tip = 0;
-//        $scope.total = 0;
-//    };
-//
-//    $scope.initEarnings = function () {
-//        $scope.tipTotal = 0;
-//        $scope.mealCount = 0;
-//    };
-//
-//    $scope.init = function () {
-//        $scope.formError = "";
-//        $scope.resetDetails();
-//        $scope.initCharges();
-//        $scope.initEarnings();
-//    };
-//
-//    $scope.init();
-//
-//    $scope.submitDetails = function () {
-//        if ($scope.enterPriceForm.$invalid) {
-//            $scope.formError = "Please enter valid values ";
-//        } else {
-//            $scope.formError = "";
-//            $scope.tipTotal += $scope.tip;
-//            $scope.mealCount++;
-//        }
-//    };
-//
-//    $scope.$watchGroup(['mealPrice', 'taxRate', 'tipPercentage'], function (newValues, oldValues, scope) {
-//        if ($scope.enterPriceForm.$invalid) {
-//            $scope.initCharges();
-//
-//        } else {
-//            $scope.formError = "";
-//            $scope.subtotal = $scope.mealPrice + ($scope.mealPrice * ($scope.taxRate / 100));
-//            $scope.tip = $scope.mealPrice * ($scope.tipPercentage / 100);
-//            $scope.total = $scope.subtotal + $scope.tip;
-//        }
-//    });
-//
-//    $scope.$watchGroup(['tipTotal', 'mealCount'], function (newValues, oldValues, scope) {
-//        if ($scope.mealCount != 0) {
-//            $scope.avgTipPerMeal = $scope.tipTotal / $scope.mealCount;
-//        } else {
-//            $scope.avgTipPerMeal = 0;
-//        }
-//
-//    });
-//
-//
-//
-//
-//}]);
